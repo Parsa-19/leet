ef8ab83b2abb280bec42b69f8f87ed4d6f928125
Parsa-19 <parsakhalife19@gmail.com>
2025-07-13 17:31:03 +0330

    add sherkat files

---
+++ isSubsequence/main.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    
+    def letter_in_t(self, letter, t):
+        return True if letter in 
+    
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
